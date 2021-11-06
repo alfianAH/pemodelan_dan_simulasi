6cb0d0bf440ca221fff5bf0e2dbc761c051a50b9
Alfian Hamdani <alfian.hamdani04@gmail.com>
2021-11-06 13:44:20 +0800

    Scaled the data

--- chapter09/Ch9.Airfoil Self-Noise.py
+++ chapter09/Ch9.Airfoil Self-Noise.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 
 def main():
@@ -8,11 +9,23 @@ def main():
     asn_data = pd.read_csv('airfoil_self_noise.dat',
                            delim_whitespace=True, names=asn_names)
 
-    print(asn_data.head())  # Melihat 5 data teratas
-    print(asn_data.info())  # Melihat info asn_data
+    # print(asn_data.head())  # Melihat 5 data teratas
+    # print(asn_data.info())  # Melihat info asn_data
 
     basic_stats = asn_data.describe()  # Melihat detail masing-masing kolom
     basic_stats = basic_stats.transpose()  # Transpose kolom dan baris
+    # print(basic_stats)
+
+    # Scaling
+    scaler_object = MinMaxScaler()
+    asn_data_scaled = scaler_object.fit_transform(asn_data)
+    asn_data_scaled = pd.DataFrame(asn_data_scaled, columns=asn_names)
+
+    summary = asn_data_scaled.describe()
+    summary = summary.transpose()
+    print(summary)
+
+
 
 
 
